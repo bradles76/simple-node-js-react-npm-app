b165465651bfd2c91e7a3e7e82be3da1fa0771ae
Adam Bradley <bradles76@hotmail.com>
2020-03-02 18:33:25 +1100

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 class App extends Component {
   render() {
     return (
